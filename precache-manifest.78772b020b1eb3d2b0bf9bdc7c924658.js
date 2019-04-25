552c22263ee19c9137066dc4e5d46933c7f7e92a
Mikhail Korzhev <mkorzhev@hotmail.com>
2019-04-25 18:42:56 +0300

    Updates

---
+++ precache-manifest.78772b020b1eb3d2b0bf9bdc7c924658.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = [
+  {
+    "revision": "78e9de8b0bb3c996f969f16d0e36fc15",
+    "url": "/static/media/8.78e9de8b.jpg"
+  },
+  {
+    "revision": "a6144f5b79eb3e6c5f66",
+    "url": "/static/css/main.196a7453.chunk.css"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "baf961a1263ccd6a2d24",
+    "url": "/static/js/2.0f58faaa.chunk.js"
+  },
+  {
+    "revision": "3e35cd7fb49f0188ea6607df7a3167dd",
+    "url": "/static/media/3.3e35cd7f.jpg"
+  },
+  {
+    "revision": "823acd57e9879245e5703ac46109b088",
+    "url": "/static/media/2.823acd57.jpg"
+  },
+  {
+    "revision": "c5410922fddb017747e205acecbe100e",
+    "url": "/static/media/1.c5410922.jpg"
+  },
+  {
+    "revision": "108dbdffef95cd434f32e827be4fd7f8",
+    "url": "/static/media/5.108dbdff.jpg"
+  },
+  {
+    "revision": "f7ed51138f3efc2604311406bf737721",
+    "url": "/static/media/4.f7ed5113.jpg"
+  },
+  {
+    "revision": "c5bf53a8979574c1d8d0ac4364e8ef64",
+    "url": "/static/media/6.c5bf53a8.jpg"
+  },
+  {
+    "revision": "ebe79a4d0fc707d2c1d616253594f90c",
+    "url": "/static/media/7.ebe79a4d.jpg"
+  },
+  {
+    "revision": "a6144f5b79eb3e6c5f66",
+    "url": "/static/js/main.441bb335.chunk.js"
+  },
+  {
+    "revision": "d5b190468dceec38e129abe5b22974ba",
+    "url": "/static/media/9.d5b19046.jpg"
+  },
+  {
+    "revision": "7d4bb175770447ef905baedd998f2a4f",
+    "url": "/static/media/10.7d4bb175.jpg"
+  },
+  {
+    "revision": "5e01b9ccb29244902d3fec71590bfa97",
+    "url": "/static/media/11.5e01b9cc.jpg"
+  },
+  {
+    "revision": "cb825bb84c4dd5170b58d75be22544c2",
+    "url": "/static/media/12.cb825bb8.jpg"
+  },
+  {
+    "revision": "3b46cd8e14f6d2e98725377ca2f13881",
+    "url": "/static/media/13.3b46cd8e.jpg"
+  },
+  {
+    "revision": "a58e7b9ffc0ea4d7305deca453336194",
+    "url": "/static/media/14.a58e7b9f.jpg"
+  },
+  {
+    "revision": "b7cb48706c9f09df7dbc9a402a5da404",
+    "url": "/static/media/15.b7cb4870.jpg"
+  },
+  {
+    "revision": "49a21dad5d12e613a9af75a887a3b7c5",
+    "url": "/static/media/16.49a21dad.jpg"
+  },
+  {
+    "revision": "8d6f084f6e7a93c82aa9779fc9f52d45",
+    "url": "/static/media/18.8d6f084f.jpg"
+  },
+  {
+    "revision": "5fcf11cfa58f999fe358311932c528e2",
+    "url": "/static/media/17.5fcf11cf.jpg"
+  },
+  {
+    "revision": "cb30f21699afb4de1ef889a3701b0d61",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/pairs-card-game/precache-manifest.ff9a80c4234182b5ddb2dba37d80b1e2.js"
+  "/precache-manifest.78772b020b1eb3d2b0bf9bdc7c924658.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/pairs-card-game/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
